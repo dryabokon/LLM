649d9a5b3b4d7bc2ee9740a98ed6e2a024741c59
dryabokon <d.ryabokon@gmail.com>
2023-10-19 14:30:12 +0300

    CI

--- test_01.py
+++ test_01.py
@@ -2,5 +2,5 @@ import time
 # ---------------------------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
     time0 = time.time()
-    with open('./artifacts/log.txt', mode='w') as f:
-        f.write('%.2f sec OK'%(time.time()-time0))
+    with open('log.txt', mode='w') as f:
+        f.write('%.2f sec: OK'%(time.time()-time0))
