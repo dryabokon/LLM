33ae4ea459b84dc735c9872f8071844e943bca1c
dryabokon <d.ryabokon@gmail.com>
2023-10-19 17:45:53 +0300

    CI

--- tests/test_00_Boundary_Tests.py
+++ tests/test_00_Boundary_Tests.py
@@ -4,6 +4,7 @@ import unittest
 import inspect
 # ----------------------------------------------------------------------------------------------------------------------
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))))
+sys.path.insert(1, './tools/')
 from ex_01a_unit_tests_codebase import json_to_pandas_v01
 # ----------------------------------------------------------------------------------------------------------------------
 folder_out = './data/output/'
